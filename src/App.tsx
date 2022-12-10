929cfc6996cb79bb6f368305174785f79d8ce774
yacine <lentente1958@gmail.com>
2022-12-10 19:15:44 +0100

    define structures

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import './App.css';
 import ButtonsArea from './components/ButtonsArea';
+import ErrorHandler from './components/Error';
+import Peace from './components/peace';
+import Progress from './components/progress';
 import Welcom from './components/welcom';
 function App() {
+ 
+  const handleInput=()=>{
+
+  }
   return  <>
   <div className="d1 main-center">
       <form method="POST" encType="multipart/form-data" > 
                 <div className="area1 center">
                     <h1>Uploading System</h1> 
-                    <label><input type="file" name="uploading[]" id="upload" multiple /></label>
+                    <label><input disabled={false} onChange={handleInput} type="file" name="uploading[]" id="upload" multiple /></label>
                 </div>
-                <div className="area2">
+                {/* <Progress /> */}
+                <div className="area2 center-h">
                       {/* <div className="area21" id="area_release"> */}
                             <Welcom /> 
+{/* <ErrorHandler/> */}
+                      {/* <Peace img="./../../icons/close.png" name={'dedd dsdfsdv sdfsfc cxc'} extention={'dds'}   /> */}
                 </div>
-                <ButtonsArea  /> 
+                {/* <ButtonsArea  />  */}
       </form>  
   </div> 
   
