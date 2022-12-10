f097a150278bf80f6762a5409e5482578840e22e
yacine <lentente1958@gmail.com>
2022-12-10 12:31:14 +0100

    define global app and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
 import './App.css';
-
+import ButtonsArea from './components/ButtonsArea';
+import Welcom from './components/welcom';
 function App() {
-  return  <></>
+  return  <>
+  <div className="d1 main-center">
+      <form method="POST" encType="multipart/form-data" > 
+                <div className="area1 center">
+                    <h1>Uploading System</h1> 
+                    <label><input type="file" name="uploading[]" id="upload" multiple /></label>
+                </div>
+                <div className="area2">
+                      {/* <div className="area21" id="area_release"> */}
+                            <Welcom /> 
+                </div>
+                <ButtonsArea  /> 
+      </form>  
+  </div> 
+  
+  </>
 }
 
 export default App;
