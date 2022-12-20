d4af346bdf0c1cb23739b9c5b07a59b337ae1f34
yacine <lentente1958@gmail.com>
2022-12-21 00:09:10 +0100

    add progress to peaces

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,30 @@ import ErrorHandler from './components/Error';
 import Peace from './components/peace';
 import Progress from './components/progress';
 import Welcom from './components/welcom';
-import ProgressPeace from './components/peace/progressPeace';
 function App() {
  
   const handleInput=()=>{
 
   }
-  return <>
+  return  <>
   <div className="d1 main-center">
       <form method="POST" encType="multipart/form-data" > 
                 <div className="area1 center">
                     <h1>Uploading System</h1> 
                     <label><input disabled={false} onChange={handleInput} type="file" name="uploading[]" id="upload" multiple /></label>
                 </div>
-                {/* <Progress /> */} 
-                <div className="area2">
+                {/* <Progress /> */}
+                <div className="area2 center-h">
                       {/* <div className="area21" id="area_release"> */}
-                            {/* <Welcom />  */}
+                            <Welcom /> 
 {/* <ErrorHandler/> */}
-                 {/* <ProgressPeace img=" " name="" size={5} fraction={5}  />   */}
-                {/* <Peace img=" " name="dsdqd" size={5}/>
-                <Peace img=" " name="dsdqd" size={5}/> */}
+                      {/* <Peace img="./../../icons/close.png" name={'dedd dsdfsdv sdfsfc cxc'} extention={'dds'}   /> */}
                 </div>
                 {/* <ButtonsArea  />  */}
       </form>  
   </div> 
   
-  </>;
+  </>
 }
 
 export default App;
