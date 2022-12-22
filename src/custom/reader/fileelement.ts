546a26ca4d36da82635086a33fd14ada4ce3b221
yacine <lentente1958@gmail.com>
2022-12-22 22:06:26 +0100

    define class controle files

---
+++ src/custom/reader/fileelement.ts
@@ -0,0 +1,33 @@
+import { ListReadersArgs } from "./listfiles";
+/**
+ * it allows to check if files respect a set of conditions 
+ * @property {boolean} status it refers to checking status (true means there is a condition not satisfied , false everything is well)
+ * @property {string | null} message message if condition not satisfied
+*/
+class  FileElement{
+    private static type:string;
+    public static size:number;
+    private status:boolean; 
+    public message:string | null=null;
+    constructor(fireWall:ListReadersArgs['fireWall']){
+         this.status=false;
+         FileElement.type=fireWall?.type as string;
+         FileElement.size=fireWall?.size as number;
+    }
+    checkSize(file:File){
+        if(file.size >  FileElement.size ){
+               this.status=true;
+               this.message="file size is not accpeted";
+        }
+    }
+    checkType(file:File){
+          if(file.type !==FileElement.type){
+               this.status=true;
+               this.message="file type is not accpeted";
+          }
+    } 
+    getStatus():boolean{
+        return  this.status;
+    }
+}
+export default FileElement;
