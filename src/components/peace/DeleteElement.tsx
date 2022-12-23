97f8ed878213799aa887c63cbac8e09d2124ba67
yacine <lentente1958@gmail.com>
2022-12-23 18:57:50 +0100

    active delete features

--- src/components/peace/DeleteElement.tsx
+++ src/components/peace/DeleteElement.tsx
@@ -1,11 +1,11 @@
 type deleteElement= {
-    
+    deletePeace:()=>void
     setBeforeDeleteState:React.Dispatch<React.SetStateAction<boolean>>
 }
-function DeleteElement({setBeforeDeleteState}:deleteElement){
+function DeleteElement({setBeforeDeleteState,deletePeace}:deleteElement){
     return <>
         <button onClick={()=>setBeforeDeleteState(prev=>!prev)} datatype='demo' type="button">Back</button>
-        <button datatype='demo' type="button">Delete</button>
+        <button onClick={deletePeace} datatype='demo' type="button">Delete</button>
     </>
 }
 export default DeleteElement;
--- src/components/peace/index.tsx
+++ src/components/peace/index.tsx
@@ -3,15 +3,15 @@ import "./index.css";
 import DeleteElement from "./DeleteElement";
 import PeacElement from "./peaceElement"; 
 
-type peace = {img:string,name:string,size:number,state:boolean}
+type peace = {img:string,name:string,size:number,state:boolean,deletePeace:()=>void}
 
-export default function Peace({img,name,size,state}:peace){
+export default function Peace({img,name,size,state,deletePeace}:peace){
     const [beforeDeleteState ,setBeforeDeleteState]=useState<boolean>(false);
     
     return <>
             <div state-value={String(state)} className="peace">
             {/*  <div className="show_p"> </div>file="${name}" type="${type}" */}
-            {beforeDeleteState ? <DeleteElement setBeforeDeleteState={setBeforeDeleteState}  /> 
+            {beforeDeleteState ? <DeleteElement deletePeace={deletePeace} setBeforeDeleteState={setBeforeDeleteState}  /> 
                                : <PeacElement state={state} img={img} name={name} size={size} setBeforeDeleteState={setBeforeDeleteState} />
             }
             </div> 
