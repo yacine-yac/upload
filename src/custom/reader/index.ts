cd4a56f2d3569a159205aef32e4e02463baa10b7
yacine <lentente1958@gmail.com>
2022-12-23 18:37:03 +0100

    define destroy method

--- src/custom/reader/index.ts
+++ src/custom/reader/index.ts
@@ -8,17 +8,27 @@ const useListReaders:ListReaders=(fireWall)=> {
     const [files,setFiles]=useState<Reader[]>([]); 
     const [rejected,setRejected]=useState<rejectedFile[]>([]);
     const fileElement=useMemo(()=>{ return new FileElement(fireWall)},[]);
-    
+    /**
+     * it allows to destroy Reader object from files state which dispared on the view
+     * this method we will be passed to  every Reader object, and this one will implement it 
+     * @param {string} name name of file which deleted 
+     */
+    const destroyFileReader=useCallback((name:string)=>{ 
+            setFiles(prev=>{
+                prev.forEach((x,y)=>{ x.name===name &&(prev.splice(y,1))});
+                return prev;
+            });  
+    },[files]);  
     const setFilesToRead= (file:File)=>{
-        const Reading=new Reader(file);  
+        const Reading=new Reader(file,destroyFileReader);  
         Reading.startRead(); 
         Reading.getProgress(setFiles);
         Reading.getResult();  
         Reading.checkError();
         Reading.endRead(); 
-        Reading.checkError(); 
+        Reading.checkError();   
         setFiles(prev=>{return [...prev,Reading]});  
-    }  
+    }   
 /* eslint-disable */
     useEffect(()=>{
             element!==null &&  Object.values(element?.files ?? {}).forEach((item:File) => {
@@ -47,5 +57,4 @@ const useListReaders:ListReaders=(fireWall)=> {
     };
     
 }
-export {useListReaders};
-export default Reader;
+export {useListReaders}; 
