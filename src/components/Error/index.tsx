6dc185bb9b7906b31754da11dc2d150653755b3e
yacine <lentente1958@gmail.com>
2022-12-31 11:15:07 +0100

    active delete After error feature

--- src/components/Error/index.tsx
+++ src/components/Error/index.tsx
@@ -1,12 +1,15 @@
 import error from './error.png';
 import "./index.css";
-function ErrorHandler({status,messageError="Error occured to upload Request"}:{status:number,messageError?:string}){
+
+type errorType={status:number,messageError?:string,onDelete:()=>void};
+
+function ErrorHandler({onDelete,status,messageError="Error occured to upload Request"}:errorType){
     return <>
           <div className="error main-center center">
              <img src={error} alt="Error"></img> 
              <h2>{status}!, {messageError}</h2>
              <section className='center-h'>
-                <button type='button'></button>
+                <button onClick={onDelete} type='button'></button>
                 <button type='button'></button> 
              </section>
           </div>
