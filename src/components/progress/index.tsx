8518afaa0a7a167b77bd4e0f66faaa3c7c0e6eed
yacine <lentente1958@gmail.com>
2023-01-01 18:04:19 +0100

    connect style ptogress to fraction

--- src/components/progress/index.tsx
+++ src/components/progress/index.tsx
@@ -1,11 +1,12 @@
 import "./index.css";
-function Progress({fraction}:{fraction:string}){
+function Progress({fraction}:{fraction:number}){
+    const perc = (100-fraction)*560/100; //565.48
     return <>
         <div className="area_prog center"> 
             <svg>
-                <circle cx="95" cy="95" strokeDasharray="7" fill="transparent" r="80"></circle>
-                <circle cx="95" cy="90" r="80"></circle>
-                <text x="75" y="110" fill="white">{fraction}</text>
+                <circle r="90" cx="100" cy="100" fill="transparent"    strokeDasharray="560" strokeDashoffset="0"></circle>
+                <circle r="90" cx="100" cy="95" strokeDasharray="565.48" strokeDashoffset={perc}></circle>
+                <text x="75" y="110" fill="white">{fraction+"%"}</text>
             </svg>  
         </div>
     </>
